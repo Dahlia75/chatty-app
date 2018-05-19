3fee0f99ba6239d59792b289844b99d709baa6d6
Dahlia75 <d.aljabari75@gmail.com>
2018-05-18 20:36:09 -0700

    fix the text with image

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
-React Boilerplate
+Chatty App
 =====================
 
-A minimal and light dev environment for ReactJS.
+A chat website app to talk to freinds and broadcast messages to all online users.
+
+## Final Product
+
+!["Screenshot of changing user name"]()
+
+!["Screenshot of sending image"]()
+
+!["Screenshot for how the image will be shown in second user browser"]()
+
+!["Screenshot of sending text with image"]()
 
 ### Usage
 
@@ -12,7 +22,6 @@ git clone git@github.com:lighthouse-labs/react-simple-boilerplate.git
 cd react-simple-boilerplate
 git remote rm origin
 git remote add origin [YOUR NEW REPOSITORY]
-# Manually update your package.json file
 ```
 
 Install the dependencies and start the server.
@@ -22,12 +31,6 @@ npm install
 npm start
 open http://localhost:3000
 ```
-
-### Static Files
-
-You can store static files like images, fonts, etc in the `build` folder.
-
-For example, if you copy a file called my_image.png into the build folder you can access it using `http://localhost:3000/build/my_image.png`.
 
 ### Linting
 
@@ -39,7 +42,19 @@ npm run lint
 
 ### Dependencies
 
-* React
-* Webpack
-* [babel-loader](https://github.com/babel/babel-loader)
-* [webpack-dev-server](https://github.com/webpack/webpack-dev-server)
+babel-core
+babel-loader
+babel-preset-es2015
+babel-preset-react
+css-loader
+node-sass
+sass-loader
+sockjs-client
+style-loader
+webpack
+webpack-dev-server
+react
+react-dom
+express
+ws
+uuid
--- src/Message.jsx
+++ src/Message.jsx
@@ -2,28 +2,34 @@ import React, { Component } from 'react';
 
 class Message extends Component {
   checkImageInMsg(){
-    const imageCheck = /^https?:\/\/.*?\.(jpe?g|png|gif)$/i;
-
-    console.log("Does text contain image: ", imageCheck.test(this.props.singleMessage.content));
-    // const splitMsg = this.props.singleMessage.content.split(imageCheck).map(function(item) {
-    //   console.log("item: ", item);
-    //   if (imageCheck.test(item)
-    //     ) {
-    //       return <img className="message-image" src={item} />
-    //     }else {
-    //       return <span className="message-content">{item}</span>
-    //     }
-
-    //     // <br/>
-    // });
-    // console.log("split: ", splitMsg);
-    if (imageCheck.test(this.props.singleMessage.content)
-    ) {
-      return <img className="message-image" src={this.props.singleMessage.content} />
-    }else {
+    //check the URL format
+    const imageCheck = /https?:\/\/.*?\.(jpe?g|png|gif)/i;
+    //Does the text contains image?
+    const isLink = word => imageCheck.test(word);
+    if (isLink(this.props.singleMessage.content)){
+      const splitMsg = this.props.singleMessage.content.split(' ')
+      .reduce((acc, current) => {
+        const last = arr => arr[arr.length - 1];
+        if (acc.length === 0 || isLink(last(acc)) || isLink(current)) {
+          acc.push(current);
+        } else {
+          acc[acc.length - 1] = last(acc) + ' ' + current;
+        }
+        return acc;
+      }, []);
+      const multipleComponents = splitMsg.map(function(item, index) {
+        console.log("item: ", item);
+        if (imageCheck.test(item)) {
+          return <img className="message-image" key={index} src={item} />
+        } else {
+          return <span className="message-content" key={index} >{item}</span>
+        }
+      });
+      console.log("split: ", multipleComponents);
+      return multipleComponents;
+    } else {
       return <span className="message-content">{this.props.singleMessage.content}</span>
     }
-    // return splitMsg;
   }
 
   render() {
@@ -31,13 +37,14 @@ class Message extends Component {
     switch(this.props.singleMessage.type){
       case "incomingMessage":
         const msgContent = this.checkImageInMsg();
-        // console.log("msgContent: ",msgContent);
         let style = {color: this.props.singleMessage.userColor};
 
         msg = (
           <div className="message">
             <span className="message-username" style={style}>{this.props.singleMessage.username}</span>
-            {msgContent}
+            <div className="message-content-wrapper">
+              {msgContent}
+            </div>
           </div>);
         break;
       case "incomingNotification":
@@ -47,7 +54,6 @@ class Message extends Component {
           </div>);
         break;
     }
-
     return msg;
   }
 }
